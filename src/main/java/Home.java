239890067c27d6a9179109b25a42a558195eed8a
Duda <dudasouza@live.com>
2023-10-01 05:42:55 -0300

    Revisao Final 2

--- src/main/java/Home.java
+++ src/main/java/Home.java
@@ -14,16 +14,18 @@ import java.util.Random;
 
 public class Home {
 
-    private int inputSize;
-    private int hiddenSize;
-    private int outputSize;
-    private double[][] inputToHiddenWeights;
-    private double[][] hiddenToOutputWeights;
-    private double[] hiddenBiases;
-    private double[] outputBiases;
+    private int pesoEntrada;
+    private int pesoOculto;
+    private int pesoSaida;
+    private double[][] entradaPesoOculto;
+    private double[][] pesoOcultoSaida;
+    private double[] pesosOcultos;
+    private double[] saidasOcultas;
     private Random random;
 
     public static void main(String[] args) {
+
+        // Leitura do CSV
         try {
             Reader reader = Files.newBufferedReader(Paths.get("data.csv"));
             CSVParser parser = new CSVParserBuilder().withSeparator(',').build();
@@ -34,55 +36,61 @@ public class Home {
 
             List<String[]> data = csvReader.readAll();
 
-            List<double[]> inputs = new ArrayList<>();
-            List<double[]> targets = new ArrayList<>();
+            //Criaçao dos Arrays
+            List<double[]> entradas = new ArrayList<>();
+            List<double[]> saidas = new ArrayList<>();
 
             for (String[] row : data) {
-                double[] input = new double[16]; // Definir inputSize com o número correto de características
+                double[] input = new double[13]; // Numero de Entrada de Dados
                 for (int i = 2; i < row.length; i++) {
                     input[i - 2] = Double.parseDouble(row[i]);
                 }
-                inputs.add(input);
+                entradas.add(input);
 
-                double[] target = new double[3]; // Definir outputSize com o número correto de saídas
+                double[] saida = new double[3]; // Definir outputSize com o número correto de saídas
 
                 // Mapear "H", "A" e "D" para valores numéricos
                 String result = row[1];
                 if ("H".equals(result)) {
-                    target[0] = 0;
-                    target[1] = 1;
-                    target[2] = 0;
+                    saida[0] = 0;
+                    saida[1] = 1;
+                    saida[2] = 0;
                 } else if ("A".equals(result)) {
-                    target[0] = 1;
-                    target[1] = 0;
-                    target[2] = 0;
+                    saida[0] = 1;
+                    saida[1] = 0;
+                    saida[2] = 0;
                 } else if ("D".equals(result)) {
-                    target[0] = 0;
-                    target[1] = 0;
-                    target[2] = 1;
+                    saida[0] = 0;
+                    saida[1] = 0;
+                    saida[2] = 1;
                 }
 
-                targets.add(target);
+                saidas.add(saida);
             }
 
-
-            int inputSize = 16; // Tamanho  de entrada
-            int hiddenSize = 10; // Tamanho da camada oculta
-            int outputSize = 3; // Tamanho de saída
-            int epochs = 1000; // Número de épocas de treinamento
-            double learningRate = 0.01; // Taxa de aprendizado
+            //Configuraçao da Previsão
+            int tamanhoEntrada = 13;      // Tamanho  de entrada
+            int cadamadaOculta = 10;      // Tamanho da camada oculta
+            int tamanhoSaida = 3;         // Tamanho de saída
+            int geracoes = 1000;          // Número de gerações de treinamento
+            double taxaAprendizado = 0.7; // Taxa de aprendizado
 
             // Treinar a rede neural
-            Home neuralNetwork = new Home(inputSize, hiddenSize, outputSize);
-            neuralNetwork.train(inputs.toArray(new double[0][0]), targets.toArray(new double[0][0]), epochs, learningRate);
+            Home home = new Home(tamanhoEntrada, cadamadaOculta, tamanhoSaida);
+            home.treinar(entradas.toArray(new double[0][0]), saidas.toArray(new double[0][0]), geracoes, taxaAprendizado);
 
-            // Fazer previsões usando a rede neural
-            double[] inputToPredict = inputs.get(0); // Substitua pelo dado de entrada que você deseja prever
-            double[] prediction = neuralNetwork.forward(inputToPredict);
+            //Listar Entradas e fazer a Previsão
+            List<double[]> previsoes = new ArrayList<>();
 
-            // Imprimir a previsão
-            System.out.println("Previsão da rede neural: " + Arrays.toString(prediction));
+            for (int i = 0; i < entradas.size(); i++) {
+                double[] inputToPredict = entradas.get(i);
+                double[] previsao = home.forward(inputToPredict);
+                previsoes.add(previsao);
+            }
 
+            System.out.println("Foram Analisadas " + entradas.size() + " entradas");
+            double[] finalResult = calcularMedia(previsoes);
+            System.out.println("Resultado final: " + Arrays.toString(finalResult));
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
@@ -92,34 +100,52 @@ public class Home {
     }
 
 
-    public Home(int inputSize, int hiddenSize, int outputSize) {
-        this.inputSize = inputSize;
-        this.hiddenSize = hiddenSize;
-        this.outputSize = outputSize;
-        inputToHiddenWeights = new double[inputSize][hiddenSize];
-        hiddenToOutputWeights = new double[hiddenSize][outputSize];
-        hiddenBiases = new double[hiddenSize];
-        outputBiases = new double[outputSize];
+    public Home(int pesoEntrada, int pesoOculto, int pesoSaida) {
+        this.pesoEntrada = pesoEntrada;
+        this.pesoOculto = pesoOculto;
+        this.pesoSaida = pesoSaida;
+        entradaPesoOculto = new double[pesoEntrada][pesoOculto];
+        pesoOcultoSaida = new double[pesoOculto][pesoSaida];
+        pesosOcultos = new double[pesoOculto];
+        saidasOcultas = new double[pesoSaida];
         random = new Random();
-        initWeights();
+        peso();
+    }
+
+    // Calcula a media de todas as Linhas
+    private static double[] calcularMedia(List<double[]> previsoes) {
+        if (previsoes.isEmpty()) {
+            return null;
+        }
+        int outputSize = previsoes.get(0).length;
+        double[] average = new double[outputSize];
+        for (double[] previsao : previsoes) {
+            for (int i = 0; i < outputSize; i++) {
+                average[i] += previsao[i];
+            }
+        }
+        for (int i = 0; i < outputSize; i++) {
+            average[i] /= previsoes.size();
+        }
+        return average;
     }
 
 
-
+    //Calcula as saídas da rede neural
     public double[] forward(double[] input) {
-        double[] hidden = new double[hiddenSize];
-        for (int i = 0; i < hiddenSize; i++) {
-            hidden[i] = hiddenBiases[i];
-            for (int j = 0; j < inputSize; j++) {
-                hidden[i] += input[j] * inputToHiddenWeights[j][i];
+        double[] hidden = new double[pesoOculto];
+        for (int i = 0; i < pesoOculto; i++) {
+            hidden[i] = pesosOcultos[i];
+            for (int j = 0; j < pesoEntrada; j++) {
+                hidden[i] += input[j] * entradaPesoOculto[j][i];
             }
             hidden[i] = sigmoid(hidden[i]);
         }
-        double[] output = new double[outputSize];
-        for (int i = 0; i < outputSize; i++) {
-            output[i] = outputBiases[i];
-            for (int j = 0; j < hiddenSize; j++) {
-                output[i] += hidden[j] * hiddenToOutputWeights[j][i];
+        double[] output = new double[pesoSaida];
+        for (int i = 0; i < pesoSaida; i++) {
+            output[i] = saidasOcultas[i];
+            for (int j = 0; j < pesoOculto; j++) {
+                output[i] += hidden[j] * pesoOcultoSaida[j][i];
             }
             output[i] = sigmoid(output[i]);
         }
@@ -127,33 +153,33 @@ public class Home {
     }
 
 
-
-    public void train(double[][] inputs, double[][] targets, int epochs, double learningRate) {
-        for (int i = 0; i < epochs; i++) {
+    // Treinamento
+    public void treinar(double[][] inputs, double[][] saidas, int geracao, double taxaAprendizado) {
+        for (int i = 0; i < geracao; i++) {
             for (int j = 0; j < inputs.length; j++) {
                 double[] output = forward(inputs[j]);
-                double[] error = new double[outputSize];
-                for (int k = 0; k < outputSize; k++) {
-                    error[k] = targets[j][k] - output[k];
+                double[] error = new double[pesoSaida];
+                for (int k = 0; k < pesoSaida; k++) {
+                    error[k] = saidas[j][k] - output[k];
                 }
-                double[] hiddenError = new double[hiddenSize];
-                for (int k = 0; k < hiddenSize; k++) {
+                double[] hiddenError = new double[pesoOculto];
+                for (int k = 0; k < pesoOculto; k++) {
                     hiddenError[k] = 0;
-                    for (int l = 0; l < outputSize; l++) {
-                        hiddenError[k] += error[l] * hiddenToOutputWeights[k][l];
+                    for (int l = 0; l < pesoSaida; l++) {
+                        hiddenError[k] += error[l] * pesoOcultoSaida[k][l];
                     }
                 }
-                double[] hidden = new double[hiddenSize];
-                for (int k = 0; k < outputSize; k++) {
-                    outputBiases[k] += learningRate * error[k];
-                    for (int l = 0; l < hiddenSize; l++) {
-                        hiddenToOutputWeights[l][k] += learningRate * error[k] * hidden[l];
+                double[] hidden = new double[pesoOculto];
+                for (int k = 0; k < pesoSaida; k++) {
+                    saidasOcultas[k] += taxaAprendizado * error[k];
+                    for (int l = 0; l < pesoOculto; l++) {
+                        pesoOcultoSaida[l][k] += taxaAprendizado * error[k] * hidden[l];
                     }
                 }
-                for (int k = 0; k < hiddenSize; k++) {
-                    hiddenBiases[k] += learningRate * hiddenError[k];
-                    for (int l = 0; l < inputSize; l++) {
-                        inputToHiddenWeights[l][k] += learningRate * hiddenError[k] * inputs[j][l];
+                for (int k = 0; k < pesoOculto; k++) {
+                    pesosOcultos[k] += taxaAprendizado * hiddenError[k];
+                    for (int l = 0; l < pesoEntrada; l++) {
+                        entradaPesoOculto[l][k] += taxaAprendizado * hiddenError[k] * inputs[j][l];
                     }
                 }
             }
@@ -161,22 +187,22 @@ public class Home {
     }
 
 
-
-    private void initWeights() {
-        for (int i = 0; i < inputSize; i++) {
-            for (int j = 0; j < hiddenSize; j++) {
-                inputToHiddenWeights[i][j] = random.nextGaussian();
+    // Peso
+    private void peso() {
+        for (int i = 0; i < pesoEntrada; i++) {
+            for (int j = 0; j < pesoOculto; j++) {
+                entradaPesoOculto[i][j] = random.nextGaussian();
             }
         }
-        for (int i = 0; i < hiddenSize; i++) {
-            for (int j = 0; j < outputSize; j++) {
-                hiddenToOutputWeights[i][j] = random.nextGaussian();
+        for (int i = 0; i < pesoOculto; i++) {
+            for (int j = 0; j < pesoSaida; j++) {
+                pesoOcultoSaida[i][j] = random.nextGaussian();
             }
         }
     }
 
 
-
+    //Função de Aprendizado
     private double sigmoid(double x) {
         return 1.0 / (1.0 + Math.exp(-x));
 
