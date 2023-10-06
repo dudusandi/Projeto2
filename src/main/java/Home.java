fa89b9dd91498a24c02b5778a3fe079741cb7586
Duda <dudasouza@live.com>
2023-10-06 05:20:30 -0300

    .

--- src/main/java/Home.java
+++ src/main/java/Home.java
@@ -7,25 +7,24 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Home {
 
-    private int inputSize;
-    private int hiddenSize;
-    private int outputSize;
-    private double[][] inputToHiddenWeights;
-    private double[][] hiddenToOutputWeights;
-    private double[] hiddenBiases;
-    private double[] outputBiases;
-    private Random random;
+    private final int inputSize;
+    private final int hiddenSize;
+    private final int outputSize;
+    private final double[][] inputToHiddenWeights;
+    private final double[][] hiddenToOutputWeights;
+    private final double[] hiddenBiases;
+    private final double[] outputBiases;
+    private final Random random;
+
+
 
     public static void main(String[] args) {
         try {
-            Reader reader = Files.newBufferedReader(Paths.get("data.csv"));
+            Reader reader = Files.newBufferedReader(Paths.get("datas.csv"));
             CSVParser parser = new CSVParserBuilder().withSeparator(',').build();
             CSVReader csvReader = new CSVReaderBuilder(reader)
                     .withCSVParser(parser)
@@ -34,20 +33,34 @@ public class Home {
 
             List<String[]> data = csvReader.readAll();
 
-            List<double[]> inputs = new ArrayList<>();
-            List<double[]> targets = new ArrayList<>();
 
-            for (String[] row : data) {
-                double[] input = new double[14]; // Definir inputSize com o número correto de características
+
+            long seed = System.nanoTime();
+            Collections.shuffle(data, new Random(seed));
+
+
+            double splitRatio = 0.7;
+            int splitIndex = (int) (data.size() * splitRatio);
+
+
+            List<String[]> trainingData = data.subList(0, splitIndex);
+            List<String[]> validationData = data.subList(splitIndex, data.size());
+
+
+            List<double[]> trainingInputs = new ArrayList<>();
+            List<double[]> trainingTargets = new ArrayList<>();
+            List<double[]> validationInputs = new ArrayList<>();
+            List<double[]> validationTargets = new ArrayList<>();
+
+            for (String[] row : trainingData) {
+                double[] input = new double[12];
                 for (int i = 2; i < row.length; i++) {
                     input[i - 2] = Double.parseDouble(row[i]);
                 }
-                inputs.add(input);
+                trainingInputs.add(input);
 
-                double[] target = new double[3]; // Definir outputSize com o número correto de saídas
-
-                // Mapear "H", "A" e "D" para valores numéricos
-                String result = row[1];
+                double[] target = new double[3];
+                String result = row[0];
                 if ("H".equals(result)) {
                     target[0] = 0;
                     target[1] = 1;
@@ -61,35 +74,57 @@ public class Home {
                     target[1] = 0;
                     target[2] = 1;
                 }
-
-                targets.add(target);
+                trainingTargets.add(target);
             }
 
+            for (String[] row : validationData) {
+                double[] input = new double[12];
+                for (int i = 2; i < row.length; i++) {
+                    input[i - 2] = Double.parseDouble(row[i]);
+                }
+                validationInputs.add(input);
 
-            int inputSize = 14; // Tamanho  de entrada
-            int hiddenSize = 10; // Tamanho da camada oculta
-            int outputSize = 3; // Tamanho de saída
-            int epochs = 1000; // Número de épocas de treinamento
-            double learningRate = 0.01; // Taxa de aprendizado
+                double[] target = new double[3];
+                String result = row[0];
+                if ("H".equals(result)) {
+                    target[0] = 0;
+                    target[1] = 1;
+                    target[2] = 0;
+                }else if ("A".equals(result)) {
+                    target[0] = 1;
+                    target[1] = 0;
+                    target[2] = 0;
+                }else if ("D".equals(result)) {
+                    target[0] = 0;
+                    target[1] = 0;
+                    target[2] = 1;
+                }
+                validationTargets.add(target);
+            }
 
-            // Treinar a rede neural
+            int inputSize = 12;             // Data Input
+            int hiddenSize = 10;             // Neurons Number
+            int outputSize = 3;             // Data Output
+            int epochs = 200;                // Epochs Training
+            double learningRate = 0.07;      // Learning Rate
+
             Home neuralNetwork = new Home(inputSize, hiddenSize, outputSize);
-            neuralNetwork.train(inputs.toArray(new double[0][0]), targets.toArray(new double[0][0]), epochs, learningRate);
+            neuralNetwork.train(trainingInputs.toArray(new double[0][0]), trainingTargets.toArray(new double[0][0]), epochs, learningRate);
 
-            // Fazer previsões usando a rede neural
-            double[] inputToPredict = inputs.get(0); // Substitua pelo dado de entrada que você deseja prever
+            double validationError = neuralNetwork.validate(validationInputs.toArray(new double[0][0]), validationTargets.toArray(new double[0][0]));
+            System.out.println("Validation Error: " + validationError);
+
+            double[] inputToPredict = validationInputs.get(0);
             double[] prediction = neuralNetwork.forward(inputToPredict);
-
-            // Imprimir a previsão
-            System.out.println("Previsão da rede neural: " + Arrays.toString(prediction));
+            System.out.println("Victory Prediction(Home Team: Away Team : Draw): " + Arrays.toString(prediction));
 
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-
     }
+
 
 
     public Home(int inputSize, int hiddenSize, int outputSize) {
@@ -105,6 +140,27 @@ public class Home {
     }
 
 
+
+
+    public double validate(double[][] inputs, double[][] targets) {
+        double totalError = 0.0;
+
+        for (int i = 0; i < inputs.length; i++) {
+            double[] output = forward(inputs[i]);
+            double[] error = new double[outputSize];
+            for (int k = 0; k < outputSize; k++) {
+                error[k] = targets[i][k] - output[k];
+            }
+            double dataPointError = 0.0;
+            for (int k = 0; k < outputSize; k++) {
+                dataPointError += Math.pow(error[k], 2);
+            }
+            dataPointError /= outputSize;
+            totalError += dataPointError;
+        }
+
+        return totalError / inputs.length;
+    }
 
     public double[] forward(double[] input) {
         double[] hidden = new double[hiddenSize];
@@ -129,6 +185,8 @@ public class Home {
 
 
     public void train(double[][] inputs, double[][] targets, int epochs, double learningRate) {
+        double totalError = 0.0;
+
         for (int i = 0; i < epochs; i++) {
             for (int j = 0; j < inputs.length; j++) {
                 double[] output = forward(inputs[j]);
@@ -136,6 +194,17 @@ public class Home {
                 for (int k = 0; k < outputSize; k++) {
                     error[k] = targets[j][k] - output[k];
                 }
+                double dataPointError = 0.0;
+                for (int k = 0; k < outputSize; k++) {
+                    dataPointError += Math.pow(error[k], 2);
+                }
+                dataPointError /= outputSize;
+                totalError += dataPointError;
+
+                double averageError = totalError / inputs.length;
+               // System.out.println("Epoch " + (i + 1) + " - Average Error: " + averageError);
+                totalError = 0.0;
+
                 double[] hiddenError = new double[hiddenSize];
                 for (int k = 0; k < hiddenSize; k++) {
                     hiddenError[k] = 0;
@@ -174,7 +243,6 @@ public class Home {
             }
         }
     }
-
 
 
     private double sigmoid(double x) {
